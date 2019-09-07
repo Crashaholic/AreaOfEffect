7021834b5718fdbea7b8441d3ad04b4b8d723392
Crashaholic <33598082+Crashaholic@users.noreply.github.com>
2019-09-07 22:55:31 +0800

    Odd gamepad issue fixed

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -17,45 +17,50 @@ void Player::InitCam(Camera * cam)
 void Player::MoveX_KB(bool dirX, double dt)
 {
 	if (dirX)
-	{
 		GO->vel.x += speed * dt;
-	}
 	else
-	{
 		GO->vel.x -= speed * dt;
-	}
+	std::cout  << GO->vel.x << '\n';
 	GO->vel.x = Math::Clamp(GO->vel.x, -maxSpeed, maxSpeed);
-	cameraAttachment->position.x = GO->pos.x;
-	cameraAttachment->target.x = GO->pos.x;
 }
 
 void Player::MoveY_KB(bool dirY, double dt)
 {
 	if (dirY)
-	{
 		GO->vel.y += speed * dt;
-	}
 	else
-	{
 		GO->vel.y -= speed * dt;
-	}
 	GO->vel.y = Math::Clamp(GO->vel.y, -maxSpeed, maxSpeed);
-	cameraAttachment->position.y = GO->pos.y;
-	cameraAttachment->target.y = GO->pos.y;
 }
 
 void Player::MoveX_Pad(float axis, double dt)
 {
-	GO->vel.x += axis * speed * dt;
+	float x;
+	if (axis > 0)
+	{
+		x = (axis - -0.8f) / (0.8f - -0.8f);
+	}
+	else
+	{
+		x = -(axis - 0.8f) / (-0.8f - 0.8f);
+	}
+	GO->vel.x += x * speed * dt;
+	std::cout << "x:\t" << x << "\t\t\t" << GO->vel.x << '\n';
 	GO->vel.x = Math::Clamp(GO->vel.x, -maxSpeed, maxSpeed);
-	cameraAttachment->position.x = GO->pos.x;
-	cameraAttachment->target.x = GO->pos.x;
 }
 
 void Player::MoveY_Pad(float axis, double dt)
 {
-	GO->vel.y -= axis * speed * dt;
+	float y;
+	if (axis > 0)
+	{
+		y = (axis - -0.8f) / (0.8f - -0.8f);
+	}
+	else
+	{
+		y = -(axis - 0.8f) / (-0.8f - 0.8f);
+	}
+	GO->vel.y -= y * speed * dt;
+	std::cout << "y:\t" << y << "\t\t\t" << GO->vel.y << '\n';
 	GO->vel.y = Math::Clamp(GO->vel.y, -maxSpeed, maxSpeed);
-	cameraAttachment->position.y = GO->pos.y;
-	cameraAttachment->target.y = GO->pos.y;
 }
